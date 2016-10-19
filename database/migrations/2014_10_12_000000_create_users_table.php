4a3ef3fe7c9334a3af1d3792338a7b67d2b1007e
Krishna Ram Prakash R <krp@gtux.in>
2016-10-19 16:59:08 +0530

    Added migration for roles

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,12 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('role_id')->unsigned();
             $table->rememberToken();
             $table->timestamps();
+
+            //Foreign Keys
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
