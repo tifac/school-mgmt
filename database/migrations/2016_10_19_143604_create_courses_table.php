eeb9225f59f43141ec488f1d045d2b895ecf11f2
Akash <akashvijayaraj44@gmil.com>
2016-10-19 21:13:32 +0530

    Added migration for courses

---
+++ database/migrations/2016_10_19_143604_create_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('course_name');
+            $table->string('course_code');
+            $table->integer('credits')->unsigned(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
