270f6c203436d6ffd5f8a768aa1c3eed64ac145a
Krishna Ram Prakash R <krp@gtux.in>
2016-11-03 01:58:43 +0530

    Added controllers for frontend views

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,5 +32,40 @@ class HomeController extends Controller
 
     public function manageStaff() {
         return view('dashboard.managestaff');
-    }    
+    }
+
+    public function viewAddStaff() {
+        return view('dashboard.editstaff');
+    }
+
+    public function viewAddStudent() {
+        return view('dashboard.editstudent');
+    }
+
+    public function addStudent(Request $request) {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'rollno' => 'required',
+            'dob' => 'required|date',
+            'address' => 'required',
+            'parent1_contact' => 'required|digits:10',
+            'parent2_contact' => 'required|digits:10',
+            'home_contact' => 'required|digits',
+            'bloodgroup' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+    }
+
+    public function addStaff(Request $request) {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'joining_date' => 'required|date',
+            'qualification' => 'required',
+            'address' => 'required',
+            'contact' => 'required|digits:10',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+    }
 }
