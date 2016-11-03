d8c1c48d8754403d5490ae8933308421dc0ebc99
Krishna Ram Prakash R <krp@gtux.in>
2016-11-03 23:47:52 +0530

    staff addition module complete

--- resources/views/dashboard/editstaff.blade.php
+++ resources/views/dashboard/editstaff.blade.php
@@ -2,26 +2,63 @@
 
 @section('content')
 
+  @if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+  @endif
+
+  @if (isset($status))
+    <div class="alert alert-success" role="alert">
+      <a href="#" class="alert-link">Successfully updated!</a>
+    </div>
+  @endif
+
   <div class="row">
       <!-- left column -->
       
       <!-- edit form column -->
 
-        <h3>Personal info</h3>
+        <h3>Add new staff</h3>
         
-        <form class="form-horizontal" role="form">
+        <form class="form-horizontal" role="form" method="post" action="{{ url('/dashboard/staff/new') }}">
 
           <div class="form-group">
             <label class="col-lg-3 control-label">Name:</label>
             <div class="col-lg-8">
-              <input class="form-control" type="text" value="" >
+              <input class="form-control" type="text" value="" name="name">
             </div>
           </div>
 
-           <div class="form-group">
-            <label class="col-lg-3 control-label">Date of Birth</label>
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Qualification:</label>
             <div class="col-lg-8">
-              <input type="date" class="form-control" name="Date of Birth">
+              <input type="date" class="form-control" name="qualification">
+            </div>
+           </div>
+
+           <div class="form-group">
+            <label class="col-lg-3 control-label">Address:</label>
+            <div class="col-lg-8">
+              <textarea name="address" class="form-control" rows="3"></textarea>
+            </div>
+           </div>
+
+           <div class="form-group">
+            <label class="col-lg-3 control-label">Contact:</label>
+            <div class="col-lg-8">
+              <input type="date" class="form-control" name="contact">
+            </div>
+           </div>
+
+           <div class="form-group">
+            <label class="col-lg-3 control-label">Date of Joining:</label>
+            <div class="col-lg-8">
+              <input type="date" class="form-control" name="joining_date">
             </div>
            </div>
 
@@ -29,14 +66,14 @@
           <div class="form-group">
             <label class="col-lg-3 control-label">Email:</label>
             <div class="col-lg-8">
-              <input class="form-control" type="text" value="">
+              <input class="form-control" type="text" value="" name="email">
             </div>
             </div>
 
            <div class="form-group">
             <label class="col-md-3 control-label">Password:</label>
             <div class="col-md-8">
-              <input class="form-control" type="password" value="">
+              <input class="form-control" type="password" value="" name="password">
             </div>
           </div>
           
@@ -50,12 +87,12 @@
           <div class="form-group">
             <label class="col-md-3 control-label"></label>
             <div class="col-md-8">
-              <input type="button" class="btn btn-primary" value="Save Changes">
+              <input type="submit" class="btn btn-primary" value="Save Changes">
               <span></span>
               <input type="reset" class="btn btn-default" value="Cancel">
             </div>
           </div>
-
+          {{ csrf_field() }}
         </form>
 
       </div>
